80e189fe610081963593721565a6a9dc1af2c5c2
tranhuyen133 <147359382+tranhuyen133@users.noreply.github.com>
2024-05-17 01:09:54 +0700

    update ss30

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import React from 'react'
-import Function from './components/functionComponent/Function'
-import Bt1 from './components/bai1/bt1'
-import Bt2 from './components/bai2/Bt2'
-import Bt3 from './components/bai3/Bt3'
+import Bt1 from './components/bt1/Bt1'
+import Bt2 from './components/bt2/Bt2'
+import Bt3 from './components/bt3/Bt3'
+import Bt4 from './components/bt4/Bt4'
+import Bt5 from './components/bt5/Bt5'
+import Bt6 from './components/bt6/Bt6'
+import Bt7 from './components/bt7/Bt7'
+import Bt8 from './components/bt8/Bt8'
+import Bt9 from './components/bt9/Bt9'
+import Bt10 from './components/bt10/Bt10'
+import Bt11 from './components/bt11/Bt11'
 export default function App() {
   return (
     <div>
-      <Function></Function>
-      <Class></Class>
       <Bt1></Bt1>
+      <br />
       <Bt2></Bt2>
-      <Bt3 ></Bt3>
+      <br />
+      <Bt3></Bt3>
+      <br />
+      <Bt4></Bt4>
+      <br />
+      <Bt5></Bt5>
+      <br />
+      <Bt6></Bt6>
+      <br />
+      <Bt7></Bt7>
+      <br />
+      <Bt8></Bt8>
+      <br />
+      <Bt9></Bt9>
+      <br />
+      <Bt10></Bt10>
+      <br />
+      <Bt11></Bt11>
+      <br />
     </div>
   )
 }
--- src/conponent/bt7/Bt7.tsx
+++ src/conponent/bt7/Bt7.tsx
@@ -2,16 +2,35 @@ import React, { useState } from 'react';
 
 export default function Bt7() {
   const [numbers, setNumbers] = useState<number[]>([]);
+  /*
+    các bước làm
+    1.Tạo ra mảng rỗng để chúa cac phan tử 
+    2.Tạo 1 button clich và bắt sự kiện click
+    3.Tạo ra sự ngẫu nhiên Math.random
+    4.push cái số ngẫu nhiên vào mảng
+  */
 
-  const add = () => {
-    const arrNumber = [1,2,3,4,5];
-    setNumbers(arrNumber);
+  const handleClick = () => {
+    // const arrNumber = [1,2,3,4,5];
+    // setNumbers(arrNumber);
+    //tạo ra số ngẫu nhiên
+    let randomNumber= Math.floor(Math.random()*9);
+    numbers.push(randomNumber);
+    console.log(11111,numbers);
+
+
+    setNumbers([...numbers])
+    
   };
-
+console.log("componet load lại");
+/*
+      Khi muốn load lại component (re-render)thì đi setState
+      bắt buộc cái State phải khác cái ban đầu 
+*/
   return (
     <div>Bt7:
-      <h2> Mảng số: [{numbers.join(', ')}]</h2>
-      <button onClick={add}>Add element</button>
+      <p>Hiển thị mảng :(number)</p>
+      <button onClick={handleClick}>addnumber</button>
     </div>
   );
 }
